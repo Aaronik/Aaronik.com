685ff721fb040fa6b07428274f80f4f701dc97a5
Aaron Sullivan <AaSullivan1618@gmail.com>
2013-08-04 17:15:07 -0400

    Initial add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AaronikCom::Application.config.secret_key_base = '0612e14a0e431edd0e49c631eec9b6d383cc5b0335aacbc6c92a34020ebf78b0b3ab9fe4126d7b580c48bee2a629c0ce1b2430fd6a047dbea6e26707bc04bf81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AaronikCom::Application.config.session_store :cookie_store, key: '_Aaronik_com_session'
