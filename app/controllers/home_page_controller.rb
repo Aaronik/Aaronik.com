31ab625e8ea9ab8fc988fddacbafedb6e2ffaae5
Aaron Sullivan <AaSullivan1618@gmail.com>
2013-08-24 18:32:27 -0400

    added db for "info"

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,10 +2,12 @@ class HomePageController < ApplicationController
 	def home
 
 		@client_ip = request.remote_ip
-		@remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
-		@my_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_addr
-		@ip_addr = request.env['REMOTE_ADDR']
-		
+
+		Info.create(ip: @client_ip)
+		#@remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
+		#@my_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_addr
+		#@ip_addr = request.env['REMOTE_ADDR']
+
 		render 'home3.html'
 	end
 end
