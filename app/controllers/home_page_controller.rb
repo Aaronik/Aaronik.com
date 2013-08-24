ecc2f18877f493c4904b7dc2e6a90b7b487dc9f6
Aaron Sullivan <AaSullivan1618@gmail.com>
2013-08-24 17:51:26 -0400

    beg ip stuff

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,5 +1,11 @@
 class HomePageController < ApplicationController
 	def home
+
+		@client_ip = request.remote_ip
+		@remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
+		@my_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_addr
+		@ip_addr = request.env['REMOTE_ADDR']
+		
 		render 'home3.html'
 	end
 end
--- app/helpers/home_page_helper.rb
+++ app/helpers/home_page_helper.rb
@@ -1,2 +1,8 @@
 module HomePageHelper
+	def get_ips
+		@client_ip = request.remote_ip
+		@remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
+		@my_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_addr
+		@ip_addr = request.env['REMOTE_ADDR']
+	end
 end
